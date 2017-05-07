f21ebba309c15c3ba11826a992277092cd40bf15
Jakub Řičař <jakub.ricar@gmail.com>
2017-05-08 01:41:01 +0200

    Improve API Test Page

--- src/pages/ApiTest.js
+++ src/pages/ApiTest.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Alert, Button, Form, FormControl, FormGroup, InputGroup, PageHeader } from 'react-bootstrap';
 import api from '../api';
 
 class ApiTestPage extends Component {
@@ -6,29 +7,117 @@ class ApiTestPage extends Component {
     super(props);
 
     this.state = {
-      isFetching: true,
-      recipes: [],
+      id: '',
+      isFetching: false,
+      data: null,
+      error: null,
     };
+
+    this.handleIdChange = this.handleIdChange.bind(this);
+    this.handleLoadListClick = this.handleLoadListClick.bind(this);
+    this.handleLoadDetailClick = this.handleLoadDetailClick.bind(this);
   }
 
-  componentWillMount() {
+  handleIdChange(event) {
+    this.setState({
+      id: event.target.value,
+    });
+  }
+
+  handleLoadListClick() {
+    this.setState({
+      isFetching: true,
+      data: null,
+      error: null,
+    });
+
     api()
       .get('/recipes')
       .then(response => this.setState({
         isFetching: false,
-        recipes: response.data,
+        data: response.data,
+      }))
+      .catch(() => this.setState({
+        isFetching: false,
+        error: 'Error fetching list.',
       }));
   }
 
+  handleLoadDetailClick() {
+    const { id } = this.state;
+
+    this.setState({
+      isFetching: true,
+      data: null,
+      error: null,
+    });
+
+    api()
+      .get(`/recipes/${id}`)
+      .then(response => this.setState({
+        isFetching: false,
+        data: response.data,
+      }))
+      .catch(() => this.setState({
+        isFetching: false,
+        error: 'Error fetching detail.',
+      }));
+  }
+
+  renderResult() {
+    const { isFetching, data, error } = this.state;
+
+    if (isFetching) {
+      return <span><i className="fa fa-spinner fa-spin" /> Loading&hellip;</span>;
+    }
+
+    if (data) {
+      return <pre>{JSON.stringify(data, ' ', 2)}</pre>;
+    }
+
+    if (error) {
+      return <Alert bsStyle="danger">{error}</Alert>;
+    }
+
+    return null;
+  }
+
   render() {
-    const { isFetching, recipes } = this.state;
+    const { id } = this.state;
 
     return (
       <div>
-        {isFetching ?
-          <span><i className="fa fa-spinner fa-spin" /> Loading&hellip;</span> :
-          <pre>{JSON.stringify(recipes)}</pre>
-        }
+        <PageHeader>API Test Page</PageHeader>
+
+        <Form inline>
+          <Button bsStyle="primary" onClick={this.handleLoadListClick}>Load List</Button>
+          {' '}
+          or
+          {' '}
+          <FormGroup>
+            <InputGroup>
+              <FormControl
+                type="text"
+                value={id}
+                onChange={this.handleIdChange}
+                placeholder="ID"
+                style={{ width: '250px' }}
+              />
+              <InputGroup.Button>
+                <Button
+                  onClick={this.handleLoadDetailClick}
+                  disabled={!id}
+                >
+                  Load Detail
+                </Button>
+              </InputGroup.Button>
+            </InputGroup>
+          </FormGroup>
+        </Form>
+
+        <br />
+
+        {this.renderResult()}
       </div>
     );
   }
