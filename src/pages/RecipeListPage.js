530cdb0cb55f497c0c8d1023c191b2a170b84b3b
Jakub Řičař <jakub.ricar@gmail.com>
2017-06-23 15:18:48 +0200

    Recipe List - search

--- src/pages/RecipeListPage.js
+++ src/pages/RecipeListPage.js
@@ -6,12 +6,56 @@ import SearchBar from '../components/SearchBar/SearchBar';
 import Spinner from '../components/common/Spinner';
 
 class RecipeListPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchText: '',
+      recipes: props.recipes,
+    };
+
+    this.handleSearchChange = this.handleSearchChange.bind(this);
+  }
+
   componentWillMount() {
     this.props.fetchRecipes();
   }
 
+  componentWillReceiveProps(nextProps) {
+    const { recipes } = nextProps;
+
+    if (this.props.recipes !== recipes) {
+      const { searchText } = this.state;
+
+      this.setState({
+        recipes: this.filterRecipes(recipes, searchText),
+      });
+    }
+  }
+
+  filterRecipes(recipes, text) {
+    if (!text) {
+      return recipes;
+    }
+
+    return recipes.filter(r =>
+      r.title.toLowerCase().includes(text.toLowerCase()),
+    );
+  }
+
+  handleSearchChange(event) {
+    const { value } = event.target;
+    const { recipes } = this.props;
+
+    this.setState({
+      searchText: value,
+      recipes: this.filterRecipes(recipes, value),
+    });
+  }
+
   render() {
-    const { recipes, isFetching } = this.props;
+    const { isFetching } = this.props;
+    const { searchText, recipes } = this.state;
 
     if (isFetching) {
       return <Spinner />;
@@ -19,7 +63,7 @@ class RecipeListPage extends Component {
 
     return (
       <div>
-        <SearchBar />
+        <SearchBar text={searchText} onChange={this.handleSearchChange} />
         <RecipeList recipes={recipes} />
       </div>
     );
